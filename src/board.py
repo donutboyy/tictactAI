0204c3fca026863a4fc1779491087ad9e995b896
Daniel Ng <47971350+donutboy929@users.noreply.github.com>
2021-11-01 11:27:03 +0800

    Revert "Ai" (#5)
    * Revert "chore: add newline to py file"
    
    This reverts commit dc5c1e3a7249e2c120df213890fc116daedb064c.
    
    * Revert "chore: add newlines to end of py files"
    
    This reverts commit 9f5bdf1dc06c946cc64e55bbe6a594f8ac7a58a7.
    
    * Revert "fix: remove accidental import"
    
    This reverts commit 80072d202e54f7257ab6e6372082db1e0ef85a05.
    
    * Revert "fix: make view board readonly and add __str__ (#3)"
    
    This reverts commit 1571ef940fe00fc693d351efd46c52a3a65d7b4b.
    
    * Revert "feat: print game board with ai with asciimatics"
    
    This reverts commit dff52513d41ed195b9876f7e8a7476825f5dee3a.
    
    * Revert "tests: ai player in a game"
    
    This reverts commit 060af5bb410f8016523fa52f2a1f8cfed2ff014a.
    
    * Revert "feat: add best move calculator with tests"
    
    This reverts commit 8d021bc1bfe912553d56505eaed2cc9d98de71f5.
    
    * Revert "test: add board tests and functions needed for minimax"
    
    This reverts commit f32290c47afd68f2048bbdaf3e166a887cfe2266.
    
    * Revert "fix: add a test for draws only when game over"
    
    This reverts commit 8a7510b964c543a8cd3532f50057d1630f8be29b.
    
    * Revert "feat: add tests for draws"
    
    This reverts commit 70769556a03f2aa467083e9a039fe7e9d40ec9a6.

--- src/board.py
+++ src/board.py
@@ -7,7 +7,8 @@ class Board():
                         "", "", ""]
                         
     def view_board(self) -> List[str]:
-        return self.__board.copy()
+        temp_board = self.__board
+        return temp_board
     
     def place_symbol(self, index, symbol) -> bool:
         if self.__board[index] == "":
@@ -16,15 +17,5 @@ class Board():
         else:
             return False
 
-    def set_board(self, boardList):
-        self.__board = boardList
 
-    def remove_symbol(self, index) -> bool:
-        if not self.__board[index] == "":
-            self.__board[index] = ""
-            return True
-        else:
-            return False
-
-    def __str__(self):
-        return self.__board.__str__()
+    
--- src/game.py
+++ src/game.py
@@ -1,5 +1,4 @@
 from player import *
-from ai_player import *
 import evaluator
 
 class Game():
@@ -19,29 +18,30 @@ class Game():
         return self.__current_player
 
     def player_turn(self, position_index) -> bool:
-        if not self.__game_over:
-            self.__current_player = self.playerX if self.__turn_counter % 2 == 0 else self.playerO
-            successfully_place_symbol = self.__current_player.place_symbol_on_board(position_index, self.board)
-            self.__turn_counter += 1 if successfully_place_symbol else 0
-            self.__game_over = evaluator.has_winner(self.board.view_board()) or evaluator.has_draw(self.board.view_board())
-            return successfully_place_symbol
-        else: return False
+        self.__current_player = self.playerX if self.__turn_counter % 2 == 0 else self.playerO
+        successfully_place_symbol = self.__current_player.place_symbol_on_board(position_index, self.board)
+        self.__turn_counter += 1 if successfully_place_symbol else 0
+        return successfully_place_symbol
         
-    def ai_player_turn(self) -> int:
-        if not self.__game_over:
-            self.__current_player = self.playerX if self.__turn_counter % 2 == 0 else self.playerO
-            bestMove = self.playerO.place_symbol_on_board(self.board)
-            self.__turn_counter += 1
-            self.__game_over = evaluator.has_winner(self.board.view_board()) or evaluator.has_draw(self.board.view_board())
-            return bestMove
-
     def get_game_over(self) -> bool:
         return self.__game_over
 
     def check_win(self) -> str:
-        winner = evaluator.get_winner(self.board.view_board())
-        self.__game_over = evaluator.has_winner(self.board.view_board()) or evaluator.has_draw(self.board.view_board())
-        return winner
+        row_winner = evaluator.check_row_win(self.board.view_board())
+        col_winner = evaluator.check_col_win(self.board.view_board())
+        diagonal_winner = evaluator.check_diagonal_win(self.board.view_board())
+        if not row_winner == "":
+            self.__game_over = True
+            return row_winner
+        elif not col_winner == "":
+            self.__game_over = True
+            return col_winner
+        elif not diagonal_winner == "":
+            self.__game_over = True
+            return diagonal_winner
+        else:
+            self.__game_over = False
+            return ""
 
     
     
